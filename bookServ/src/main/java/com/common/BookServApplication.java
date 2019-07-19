dc23f3a2ae8fcff3a37c6e751c72df54da3d49d3
Kim youngwoo <38499381+Andrewkim92@users.noreply.github.com>
2019-07-19 20:45:12 +0900

    first commit

---
+++ bookServ/src/main/java/com/common/BookServApplication.java
@@ -0,0 +1,13 @@
+package com.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServApplication.class, args);
+	}
+
+}
---
+++ bookServ/src/main/java/com/common/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.common.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
+
